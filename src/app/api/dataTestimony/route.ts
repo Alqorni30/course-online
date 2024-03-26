96391ae907a84f06bf4af42f64d4d16b83b2cdd2
Alqorni30 <vinnbayo@gmail.com>
2024-03-26 22:35:49 +0700

    change google scret dan client

--- src/app/api/dataTestimony/route.ts
+++ src/app/api/dataTestimony/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { dataTesti } from "@prisma/client";
 import db from "../../../libs/db";
+
 export const POST = async (req: Request) => {
     const body: dataTesti = await req.json();
     const dataTesti = await db.dataTesti.create({
